a01c70ef4736204fe0996324a54721bdf1784524
Karol Tomala <ktomala@golem.network>
2019-11-19 13:21:43 +0100

    Reorganization

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""Setup for tcutils."""
+
+from setuptools import setup, find_packages
+# from babel.messages import frontend as babel
+
+
+# with open('README.rst') as f:
+#     readme = f.read()
+#
+# with open('LICENSE') as f:
+#     license = f.read()
+
+setup(
+    name='tcutils',
+    version='0.1.0',
+    description='Tropicoders Utility Library',
+    # long_description=readme,
+    author='Karol Tomala',
+    author_email='ktomala@tropicoders.pl',
+    # url='https://your_project.com',
+    # license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    # cmdclass={
+    #     'compile_catalog': babel.compile_catalog,
+    #     'extract_messages': babel.extract_messages,
+    #     'init_catalog': babel.init_catalog,
+    #     'update_catalog': babel.update_catalog
+    # }
+)
