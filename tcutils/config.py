80afff05924095762cf6651824c6bb78dc0c8b41
Karol Tomala <ktomala@tropicoders.pl>
2020-10-20 13:40:17 +0200

    Add copyright notices

--- tcutils/config.py
+++ tcutils/config.py
@@ -2,6 +2,9 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2019-2020 TropiCoders Karol Tomala
+#
 
 # Cannot be used with Python3.6
 #from __future__ import annotations
