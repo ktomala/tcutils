7135e0ea8c3343ce585a2127a106af2e4d49f44e
Karol Tomala <ktomala@tropicoders.pl>
2020-10-26 11:39:19 +0100

    Prepare for publishing

---
+++ README.md
@@ -0,0 +1,12 @@
+# TropiCoders Utility Library
+
+## Overview
+
+`tcutils` is a Python>=3.6 library that defines code for
+some common operations like path manipulation, file system operations,
+packaging, configuration, schema definitions, function utilities,
+YAML extensions, etc.
+
+## License
+
+Mozilla Public License v2 (see LICENSE file).
--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ from setuptools import setup, find_packages
 # from babel.messages import frontend as babel
 
 
-# with open('README.rst') as f:
-#     readme = f.read()
+with open('README.md') as f:
+    readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
@@ -26,14 +26,20 @@ install_requires = [
 
 setup(
     name='tcutils',
-    version='0.1.1',
+    version='0.2.0',
     description='TropiCoders Utility Library',
-    # long_description=readme,
+    long_description=readme,
+    long_description_content_type="text/markdown",
     author='Karol Tomala',
     author_email='ktomala@tropicoders.pl',
     url='https://github.com/ktomala/tcutils',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
+    classifiers=[
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
     # cmdclass={
     #     'compile_catalog': babel.compile_catalog,
     #     'extract_messages': babel.extract_messages,
