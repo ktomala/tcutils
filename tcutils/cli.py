224cf7d37fb10d2a954a71143c5c4778220bd5f8
Karol Tomala <ktomala@tropicoders.pl>
2020-10-26 11:28:50 +0100

    Add NotRequiredIf to cli

--- tcutils/cli.py
+++ tcutils/cli.py
@@ -12,6 +12,32 @@ import types
 from tcutils.types import ClickGroupOrCommand
 
 
+class NotRequiredIf(click.Option):
+    def __init__(self, *args, **kwargs):
+        self.not_required_if = kwargs.pop('not_required_if')
+        assert self.not_required_if, "'not_required_if' parameter required"
+        kwargs['help'] = (kwargs.get('help', '') +
+            ' NOTE: This argument is mutually exclusive with %s' %
+            self.not_required_if
+        ).strip()
+        super(NotRequiredIf, self).__init__(*args, **kwargs)
+
+    def handle_parse_result(self, ctx, opts, args):
+        we_are_present = self.name in opts
+        other_present = self.not_required_if in opts
+
+        if other_present:
+            if we_are_present:
+                raise click.UsageError(
+                    "Illegal usage: `%s` is mutually exclusive with `%s`" % (
+                        self.name, self.not_required_if))
+            else:
+                self.prompt = None
+
+        return super(NotRequiredIf, self).handle_parse_result(
+            ctx, opts, args)
+
+
 def cli_add_groups(
     parent: ClickGroupOrCommand,
     group_module: types.ModuleType
